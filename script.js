06d0ea94d62c67acb710d9e83d0288479aa3506f
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2023-10-18 11:30:49 +0300

    Update script.js
    Practice

--- script.js
+++ script.js
@@ -64,3 +64,29 @@ const getCountryData = function (country) {
 btn.addEventListener('click', function () {
   getCountryData('romania');
 });
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening 🔮');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You win 💰');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('I waited for 1 second'));
