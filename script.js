fa6bab1188cb70858f8140935e6e6618c63262f4
Anca Preoteasa <114996344+AncaPreoteasa@users.noreply.github.com>
2023-10-18 12:39:15 +0300

    Course Challenge

--- script.js
+++ script.js
@@ -1,82 +1,82 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+// ///////////////////////////////////////
 
-const renderCountry = function (data, className = '') {
-  const html = `
-<article class="country ${className}">
-<img class="country__img" src="${data.flag}" />
-<div class="country__data">
-  <h3 class="country__name">${data.name}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
-    1
-  )}</p>
-  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-  <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-</div>
-</article>`;
+// const renderCountry = function (data, className = '') {
+//   const html = `
+// <article class="country ${className}">
+// <img class="country__img" src="${data.flag}" />
+// <div class="country__data">
+//   <h3 class="country__name">${data.name}</h3>
+//   <h4 class="country__region">${data.region}</h4>
+//   <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
+//     1
+//   )}</p>
+//   <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//   <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+// </div>
+// </article>`;
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  // countriesContainer.style.opacity = 1;
-};
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   // countriesContainer.style.opacity = 1;
+// };
 
-const request = fetch('https://restcountries.com/v2/name/romania');
+// const request = fetch('https://restcountries.com/v2/name/romania');
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-  // countriesContainer.style.opacity = 1;
-};
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText('beforeend', msg);
+//   // countriesContainer.style.opacity = 1;
+// };
 
-const getJSON = function (url, errorMsg = 'Something went wrong') {
-  return fetch(url).then(response => {
-    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
-    return response.json();
-  });
-};
+// const getJSON = function (url, errorMsg = 'Something went wrong') {
+//   return fetch(url).then(response => {
+//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+//     return response.json();
+//   });
+// };
 
-const getCountryData = function (country) {
-  getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
-    .then(data => {
-      renderCountry(data[0]);
-      const neighbor = data[0].borders[0];
+// const getCountryData = function (country) {
+//   getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbor = data[0].borders[0];
 
-      if (!neighbor) throw new Error('No neighbor found!');
+//       if (!neighbor) throw new Error('No neighbor found!');
 
-      return getJSON(
-        `https://restcountries.com/v2/alpha/${neighbor}`,
-        'Country not found'
-      );
-    })
-    .then(data => renderCountry(data, 'neighbor'))
-    .catch(err => {
-      console.log(`${err} 💥💥💥`);
-      renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
-};
+//       return getJSON(
+//         `https://restcountries.com/v2/alpha/${neighbor}`,
+//         'Country not found'
+//       );
+//     })
+//     .then(data => renderCountry(data, 'neighbor'))
+//     .catch(err => {
+//       console.log(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
 
-btn.addEventListener('click', function () {
-  getCountryData('romania');
-});
+// btn.addEventListener('click', function () {
+//   getCountryData('romania');
+// });
 
-const lotteryPromise = new Promise(function (resolve, reject) {
-  console.log('Lottery draw is happening 🔮');
-  setTimeout(function () {
-    if (Math.random() >= 0.5) {
-      resolve('You win 💰');
-    } else {
-      reject(new Error('You lost your money 💩'));
-    }
-  }, 2000);
-});
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('Lottery draw is happening 🔮');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You win 💰');
+//     } else {
+//       reject(new Error('You lost your money 💩'));
+//     }
+//   }, 2000);
+// });
 
-lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
 
 const wait = function (seconds) {
   return new Promise(function (resolve) {
@@ -84,9 +84,49 @@ const wait = function (seconds) {
   });
 };
 
-wait(2)
-  .then(() => {
-    console.log('I waited for 2 seconds');
-    return wait(1);
+// wait(2)
+//   .then(() => {
+//     console.log('I waited for 2 seconds');
+//     return wait(1);
+//   })
+//   .then(() => console.log('I waited for 1 second'));
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(4);
   })
-  .then(() => console.log('I waited for 1 second'));
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(4);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.log(error));
